4c367b7c68776110b17a7586d38d7cca51cf84fe
Nico Wong <nicowong8@gmail.com>
2021-02-25 20:24:42 -0800

    finished server for mid check

--- server.py
+++ server.py
@@ -1,7 +1,53 @@
 
 import socket
+import threading
 import sys
 import threading
+
+def broadcastToServers(msg):
+    for addr in serverAddresses:
+        mySock.sendto(msg.encode("ascii"), addr)
+        print(f"Sent message \"{msg}\" to server at port {addr[1]}")
+
+def DEBUG():
+    print(f"num of running threads: {threading.active_count()}")
+
+def handleUserInput():
+    while True:
+        cmdArgs = input().split()
+
+        cmd = cmdArgs[0]
+
+        if len(cmdArgs) == 1:
+            if cmd == "debug":
+                DEBUG()
+
+            elif cmd == "broadcastTest":
+                broadcastTest()
+
+        elif len(cmdArgs) == 2:
+            if cmd == "broadcast":
+                msg = cmdArgs[1]
+                broadcastToServers(msg)
+
+        elif len(cmdArgs) == 3:
+            if cmd == "send":    # send <msg> <port>
+                msg = cmdArgs[1].encode()
+                recipient = (socket.gethostname(), int(cmdArgs[2]) )
+
+                mySock.sendto(msg, recipient)
+
+def handleIncomingMsg(msg, addr):
+    print(f"Received message \"{msg}\" from machine at {addr}")
+    
+    # Determine whether from client or server
+    if addr in serverAddresses:
+        # Handle server msg
+        pass
+
+    else:
+        # Handle client msg
+        pass
 
 
 basePort = 8000
@@ -14,58 +60,26 @@ if len(sys.argv) != 2:
 serverID = int(sys.argv[1])
 serverPort = basePort + serverID
 
-currServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-currServer.bind((socket.gethostname(), serverPort))
-print(f"server listening on {(socket.gethostname(), serverPort)}")
+# My socket
+mySock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+mySock.bind( (socket.gethostname(), serverPort) )
+print("Started server at port", serverPort)
 
-# Print initialization
-print("Started server on port", serverPort)
-
-# .sendto() .recvfrom()
+# Addresses of other servers
 numServers = 3
-serverAddressesOut = []
+serverAddresses = []
 for i in range(numServers - 1):
     serverPort = basePort + 1 + ((serverID + i) % numServers)
-    serverAddr = (socket.gethostname(), serverPort)
+    serverAddr = (socket.gethostbyname(socket.gethostname() ), serverPort)
     # serverSocketOut = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # serverSocketOut.connect( (socket.gethostname(), serverPort) )
     print("Added outgoing server address", serverAddr)
-    serverAddressesOut.append(serverAddr)
-
-
-def broadcastTest():
-    testMessage = f"test message from serverID {serverID}"
-
-    for server in serverAddressesOut:
-        currServer.sendto(testMessage.encode("ascii"), server)
-        print(f"sent test message to server operating on port {server[1]}")
-        # threading.Thread(target=handleIncoming, args=(
-        #     server,), daemon=True).start()
-
-
-def handleIncoming(message, addr):
-    print(f"Server accepted message from another server on port {addr[1]}")
-    print(f"message: {message}")
-
-
-def DEBUG():
-    print(f"num of running threads: {threading.active_count()}")
-
-
-def handleUserInput():
-    while True:
-        command = input()
-
-        if (command == "connect"):
-            broadcastTest()
-
-        if (command == "DEBUG"):
-            DEBUG()
-
+    serverAddresses.append(serverAddr)
 
 threading.Thread(target=handleUserInput, daemon=True).start()
 
+# Poll for incoming messages
 while True:
-    data, addr = currServer.recvfrom(4096)
-    threading.Thread(target=handleIncoming, args=(
-        data.decode("ascii"), addr), daemon=True).start()
+    data, addr = mySock.recvfrom(4096)
+    threading.Thread(target=handleIncomingMsg, args=(data.decode("ascii"), addr), daemon=True).start()
+
