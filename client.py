00a72332cd3c0437b1fd3f8bcdfd2e28ea958310
Nico Wong <nicowong8@gmail.com>
2021-03-09 23:46:19 -0800

    progress, untested

--- client.py
+++ client.py
@@ -1,14 +1,21 @@
 
 import socket
 import threading
+import signal
 import queue
 import sys
 
 from Operation import Operation
+from TimeoutException import TimeoutException
 
 class Client:
 
     basePort = 7000
+    serverBasePort = 8000
+    numServers = 3
+
+    nominationTimeout = 4   # Timeout for receiving a nomination result response
+    queryResponseTimeout = 5     # Timeout for receiving a response after sending a query
     
     def __init__(self, clientID):
         cls = self.__class__
@@ -16,7 +23,13 @@ class Client:
         # Address variables
         self.ID = clientID
         self.port = cls.basePort + self.ID
-        
+
+        # Query/response variables
+        self.leaderAddress = (socket.gethostbyname(socket.gethostname() ), cls.serverBasePort + 1)    # First leader hint is Server 1
+        self.operationQueue = queue.Queue()
+        self._response = None
+
+    def start(self):
         # Setup my socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind( (socket.gethostname(), self.port) )
@@ -24,6 +37,91 @@ class Client:
 
         # Concurrently handle incoming messages
         threading.Thread(target=self.handleIncomingMessages, daemon=True).start()
+
+        # Concurrently process the operation queue
+        threading.Thread(target=self.processOperationQueue, daemon=True).start()
+
+    def processOperationQueue(self):
+        cls = self.__class__
+    
+        while True:
+            # Wait until an operation needs to be sent
+            while not len(self.operationQueue):
+                continue
+
+            #################################
+            ### Nominate a new leader ###
+            #################################
+
+            self.nominateNextLeader()
+
+            ##############################################
+            ### Start sending operations to the leader ###
+            ##############################################
+            
+            signal.alarm(cls.queryResponseTimeout)  # Timeout for a query response
+
+            while len(self.operationQueue):
+                self.sendServer(self.operationQueue.queue[0] )  # Send operation    [TODO] Send req ID with query
+
+                try:
+                    queryResponse = self.waitForResponse()
+
+                    # Received a response, so...
+                    signal.alarm(0) # Cancel the timer
+                    self.operationQueue.get()   # Pop the operation queue
+
+                    print(f"[CLIENT] Received query response: {queryResponse}")
+                    
+                except TimeoutException:
+                    print("Timed out with no query response.")
+                    break   # Assume the leader failed, so restart the process
+
+    def nominateNextLeader(self):
+        """
+        Returns once a successful nomination response is received.
+        If it returns, then self.leaderAddress is set correctly.
+        """
+        
+        # Nominate servers until one responds with a successful election result
+        while True:
+            print("Nominating next server to be leader.")
+            self.response = None    # Clear the response holder
+            signal.alarm(timeout)   # Set the timeout
+
+            self.sendServer("leader")   # Send nomination
+
+            try:
+                electionResult = self.waitForResponse()
+                signal.alarm(0) # Cancel the timer
+
+                if electionResult == "success":
+                    return
+
+                elif electionResult == "failure":
+                    # Move onto another server
+                    serverID = self.leaderAddress[1] - cls.serverBasePort
+                    nextServerPort = cls.serverBasePort + 1 + serverID % cls.numServers
+                    self.leaderAddress = (socket.gethostbyname(socket.gethostname() ), nextServerPort)
+                    print("Election failed.")
+                    continue
+                    
+                else:
+                    print("Received unknown nomination result")
+                    continue
+                    
+            except TimeoutException:
+                print("Timed out with no nomination response.")
+                continue
+          
+    def waitForResponse(self):
+        "Blocks until a response is received (from the leader)"
+        while self._response == None:    # Blocks until self.response is set
+            pass
+        return self._response
+
+    def sendServer(self, *msgTokens):
+        self.sendMessage(msgTokens, self.leaderServerAddress)
 
     def sendMessage(self, msgTokens, destinationAddr):
         msgTokenStrings = [ str(token)
@@ -39,6 +137,24 @@ class Client:
             msg = data.decode()
             print(f"Received message \"{msg}\" from machine at {addr}")
 
+            # Parse message into components
+            msgArgs = msg.split('-')
+            msgClass = msgArgs[0]
+
+            if addr == self.leaderServerAddress:
+                # Communicating with leader
+
+                if len(msgArgs) == 2:
+                    if msgClass == "response":
+                        self.response = msgArgs[1]
+
+                    else:
+                        print("Invalid message format")
+
+            else:
+                print("Invalid message format")
+
+
 def handleUserInput():
     while True:
         cmdArgs = input().split()
@@ -49,7 +165,7 @@ def handleUserInput():
             if cmd == "get":    # get <key>
                 key = cmdArgs[1]
                 op = Operation.Get(key)
-                operationQueue.put(op)
+                client.operationQueue.put(op)
                 # print("New operation queue: ", operationQueue.queue)
         
         elif len(cmdArgs) == 3:
@@ -57,7 +173,7 @@ def handleUserInput():
                 key = cmdArgs[1]
                 value = cmdArgs[2]
                 op = Operation.Put(key, value)
-                operationQueue.put(op)
+                client.operationQueue.put(op)
                 # print("New operation queue: ", operationQueue.queue)
 
             elif cmd == "send": # send <msg> <port>
@@ -77,8 +193,7 @@ clientID = int(sys.argv[1])
 
 # Start client
 client = Client(clientID)
-
-operationQueue = queue.Queue()
+client.start()
 
 # Handle stdin
 handleUserInput()
